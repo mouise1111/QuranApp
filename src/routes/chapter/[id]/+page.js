efdf999b8d00acdf28807585893b1bf9f8fb087a
Mouise Bashir <74969971+mouise1111@users.noreply.github.com>
2023-01-29 13:15:53 +0100

    added api requests

--- src/routes/chapter/[id]/+page.js
+++ src/routes/chapter/[id]/+page.js
@@ -4,8 +4,24 @@ export const load = ({ fetch, params }) => {
 		const respData = await resp.json(resp);
 		return respData;
 	};
+	const fetchVersesEnglish = async (id) => {
+		const resp = await fetch(
+			`https://api.quran.com/api/v4/verses/by_chapter/${id}?language=en&words=true`
+		);
+		const respData = await resp.json(resp);
+		return respData;
+	};
+	const fetchVersesArabic = async (id) => {
+		const resp = await fetch(
+			`https://api.quran.com/api/v4/quran/verses/indopak?chapter_number=${id}`
+		);
+		const respData = await resp.json(resp);
+		return respData;
+	};
 
 	return {
-		chapterDetail: fetchChapter(params.id)
+		chapterDetail: fetchChapter(params.id),
+		verseDetailEnglish: fetchVersesEnglish(params.id),
+		verseDetailArabic: fetchVersesArabic(params.id)
 	};
 };
