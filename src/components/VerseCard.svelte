b66a9ca70a211596dbf52a0b6ae6f4368d0e8e01
Mouise Bashir <74969971+mouise1111@users.noreply.github.com>
2023-01-30 09:30:31 +0100

    added components for verses

---
+++ src/components/VerseCard.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let verses;
+</script>
+
+<div class="flex flex-col p-5 bg-gray-800 rounded-lg m-20 border border-accent">
+	{#each verses.search.results as verse}
+		<div class="my-4 border-b border-ghost p-3 py-5">
+			<p class="text-sm font-medium text-gray-600">{verse.verse_key}</p>
+			<p>
+				{#each verse.words as words}
+					<span>
+						{words.text}
+					</span>
+				{/each}
+			</p>
+		</div>
+	{/each}
+</div>
--- src/components/Verses_Arabic.svelte
+++ src/components/Verses_Arabic.svelte
@@ -2,7 +2,7 @@
 	export let verses;
 </script>
 
-<div class="flex flex-col p-5 bg-gray-800 rounded-lg m-20 text-right">
+<div class="flex flex-col p-5 bg-gray-800 rounded-lg m-10 text-right">
 	{#each verses as verse}
 		<div class="my-4">
 			<p class="text-sm font-medium text-gray-600">{verse.verse_key}</p>
