e7875f245b286915ce7d3d239ccafd70947d65e1
Mouise Bashir <74969971+mouise1111@users.noreply.github.com>
2023-01-30 09:31:08 +0100

    changes to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,9 @@
-<h1 class="text-5xl font-bold mt-0 mb-6 text-center py-3">About this website</h1>
+<div class="py-10">
+	<h1 class="text-5xl font-bold mt-0 mb-6 text-center py-3">About this website</h1>
+	<p class="p-3 text-center">This website is inspired by Quran.com and it uses it's API 📓</p>
+	<p class="p-3 text-center">Frontend is made with SvelteKit 1.0</p>
+	<p class="p-3 text-center pb-10">Tailwind for styling 🌞</p>
+	<p class="p-3 text-center">
+		Developed by <a href="http://github.com/mouise1111" class="link">Mouise Bashir</a>
+	</p>
+</div>
